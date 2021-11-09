cd425d33145efd0bf5cc06c3a00874b1897a01b2
kennarddh <kennarddh@gmail.com>
2021-11-09 14:55:34 +0700

    remove music_loop commands due so many error

--- components/music.py
+++ components/music.py
@@ -446,21 +446,6 @@ class Music(commands.Cog):
         ctx.voice_state.songs.remove(index - 1)
         await ctx.message.add_reaction('✅')
 
-    @commands.command(name='music_loop')
-    @commands.has_role('Petabyte bot manager')
-    @commands.has_role('Verified')
-    async def _loop(self, ctx: commands.Context):
-        """Loops the currently playing song.
-        Invoke this command again to unloop the song.
-        """
-
-        if not ctx.voice_state.is_playing:
-            return await ctx.send('Nothing being played at the moment.')
-
-        # Inverse boolean value to loop and unloop.
-        ctx.voice_state.loop = not ctx.voice_state.loop
-        await ctx.message.add_reaction('✅')
-
     @commands.command(name='music_play')
     @commands.has_role('Petabyte bot manager')
     @commands.has_role('Verified')
