d6f99275ca811a91819060eb9c42280bc3a557eb
kennarddh <kennarddh@gmail.com>
2021-11-09 23:52:40 +0700

    change dick_keys to list type

--- components/translate/translate.py
+++ components/translate/translate.py
@@ -13,19 +13,12 @@ class Translate(commands.Cog):
     
     @commands.command(name='translate')
     @commands.has_role('Verified')
-    async def translate(self, ctx, destination: str, *text):
+    async def translate(self, ctx, source: str, destination: str, *text):
         """
         Translate message and edit old message.
 
         Source language detect language use 'auto'.
         """
-
-        print(language.keys())
-        print(country_code)
-        print(destination, text)
-        print(destination.lower() not in language.keys())
-        print(destination.lower() not in country_code)
-
         translator = Translator()
 
         _source = 'auto'
@@ -35,15 +28,33 @@ class Translate(commands.Cog):
         if len(' '.join(text[:])) <= 0:
             return await ctx.send('Text is a required argument that is missing.')
 
-        if destination.lower() not in language.keys():
-            return await ctx.send('Invalid destination language')
-        elif destination.lower() not in country_code:
-            return await ctx.send('Invalid destination language')
+        if not source.lower():
+            _source = translator.detect(' '.join(text[:]))
         else:
-            if destination.lower() in country_code:
-                _destination = destination
+            if source.lower() == 'auto':
+                _source = 'auto'
+            elif source.lower() not in list(language.keys()):
+                return await ctx.send('Invalid source language')
+            elif source.lower() not in country_code:
+                return await ctx.send('Invalid source language')
             else:
-                _destination = language[destination]
+                if source.lower() in country_code:
+                    _source = source
+                else:
+                    _source = language[source]
+
+        if not destination.lower():
+            _destination = destination
+        else:
+            if destination.lower() not in list(language.keys()):
+                return await ctx.send('Invalid destination language')
+            elif destination.lower() not in country_code:
+                return await ctx.send('Invalid destination language')
+            else:
+                if destination.lower() in country_code:
+                    _destination = destination
+                else:
+                    _destination = language[destination]
 
         result = translator.translate(' '.join(text[:]), dest=_destination, src=_source)
 
